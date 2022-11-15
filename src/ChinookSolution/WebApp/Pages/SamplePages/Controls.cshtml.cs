2fdd587399660084ee2baa8cba5fa573b3a044ef
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-11-15 11:38:54 -0700

    Controls Checkbox, list

--- src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -13,10 +13,21 @@ namespace WebApp.Pages.SamplePages
         public string EmailText { get; set; }
         [BindProperty]
         public string PasswordText { get; set; }
+
+        [BindProperty] public DateTime DateText { get; set; } = DateTime.Now;
+        [BindProperty] public TimeSpan TimeText { get; set; } = DateTime.Now.TimeOfDay;
+
         [BindProperty]
-        public DateTime DateText { get; set; }
+        public string Meal { get; set; }
+        //  Assume this array is actually data retrieve from the database
+        public string[] Meals { get; set; } = new[] { "breakfast", "lunch", "dinner", "snacks" }; 
+
         [BindProperty]
-        public TimeSpan TimeText { get; set; }
+        public bool AcceptanceBox { get; set; }
+
+        [BindProperty]
+        public string MessageBody { get; set; }
+
 
 
 
@@ -29,6 +40,12 @@ namespace WebApp.Pages.SamplePages
         public IActionResult OnPostTextBox()
         {
             Feedback = $"Email {EmailText}; Password {PasswordText}; Date {DateText}; Time {TimeText}";
+            return Page();
+        }
+
+        public IActionResult OnPostRadioCheckArea()
+        {
+            Feedback = $"Meal {Meal}; Acceptance {AcceptanceBox}; Message {MessageBody}";
             return Page();
         }
 
