f2782e5bd1138755ac468dd667858f421dc18d79
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-11-16 09:45:06 -0700

    Viewmodels

---
+++ src/ChinookSolution/ChinookSystem/BLL/AboutServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using ChinookSystem.ViewModels;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Entities;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class AboutServices
+    {
+        //  This class needs to be accessed by an "outside user" (WebApp)
+        //  Therefore, the class needs to be public
+
+        #region Constructor and COntext Dependency
+
+        private readonly Chinook2018Context _context;
+        internal AboutServices(Chinook2018Context context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Services
+
+        //  Services are methods
+
+        //  Query to obtain the DbVersion data
+        public DbVersionInfo GetDbVersion()
+        {
+            DbVersionInfo info = _context.DbVersions
+                .Select(x => new DbVersionInfo
+                    {
+                        Major = x.Major,
+                        Minor = x.Minor,
+                        Build = x.Build,
+                        ReleaseDate = x.ReleaseDate
+                    })
+                .SingleOrDefault();
+            return info;
+        }
+
+        #endregion
+    }
+}
---
+++ src/ChinookSolution/ChinookSystem/ViewModels/DbVersionInfo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class DbVersionInfo
+    {
+        //  The view that is used by the "outside world"
+        //  Access mst match the meth0d where the class is used (typically public)
+        //  Propose:    Use to simply carry data
+        //              Created data fields as auto-implemented properties
+        //              Consist of the "raw" data on the query
+        //              No validation done here.
+
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
